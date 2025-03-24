e07ee4ec26c36dc7d304dec309efdce67b2dcdb2
Adm. Jesus <134287070+jesusmjunior@users.noreply.github.com>
2025-03-24 17:47:14 -0300

    Update Drm.py

--- Drm.py
+++ Drm.py
@@ -34,7 +34,7 @@ def load_csv_sheet():
     return df
 
 if login():
-    st.title("📊 Painel Consolidado DECLARAÇÃO DE RECEITA MENSAL 2024 COMPLETO TODOS OS .OS(PLANILHA) E TODOS OS (PDF) ESTÃO SENDO TRATADO E VÃO ALEIMENTAR UMA SO PLANILHA E DASHBOARD (OS DADOS VEM DIRETO DA PASTA DA CAC ")
+    st.title("📊 Painel Consolidado DECLARAÇÃO DE RECEITA MENSAL 2024 COMPLETO TODOS OS .ODS(PLANILHA) E TODOS OS (PDF) ESTÃO SENDO TRATADO E VÃO ALEIMENTAR UMA SO PLANILHA E DASHBOARD (OS DADOS VEM DIRETO DA PASTA DA CAC ")
 
     df_estrutura = load_csv_sheet()
     st.success("✅ Dados carregados com sucesso!")
